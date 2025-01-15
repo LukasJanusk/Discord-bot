d8ec8194a6ece6e155d994ceaf310fe3ebeca9aa
Lukas Januskevicius <lukasjanuskevicius@Lukass-MacBook-Pro.local>
2025-01-15 13:22:16 +0200

    feat(db): created migration scripts for creating initial tables for the project: user, sprint, template, message

--- src/database/migrations/20240114T132230Z-createMessageTable.ts
+++ src/database/migrations/20240114T132230Z-createMessageTable.ts
@@ -2,12 +2,38 @@ import { Kysely, SqliteDatabase } from 'kysely';
 
 export async function up(db: Kysely<SqliteDatabase>) {
   await db.schema
-    .createTable('message')
+    .createTable('user')
+    .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
+    .addColumn('discord_name', 'text', (c) => c.notNull())
+    .execute();
+  await db.schema
+    .createTable('sprint')
+    .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
+    .addColumn('name', 'text', (c) => c.notNull())
+    .execute();
+  await db.schema
+    .createTable('template')
     .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
     .addColumn('title', 'text', (c) => c.notNull())
+    .addColumn('template_text', 'text', (c) => c.notNull())
+    .execute();
+  await db.schema
+    .createTable('message')
+    .addColumn('id', 'integer', (c) => c.primaryKey().autoIncrement().notNull())
+    .addColumn('user_id', 'integer', (c) => c.notNull().references('user.id'))
+    .addColumn('sprint_id', 'integer', (c) =>
+      c.notNull().references('sprint.id'),
+    )
+    .addColumn('template_id', 'integer', (c) =>
+      c.notNull().references('template.id'),
+    )
+    .addColumn('sent_at', 'timestamp', (c) => c.notNull())
     .execute();
 }
 
 export async function down(db: Kysely<SqliteDatabase>) {
-  await db.schema.dropTable('article').execute();
+  await db.schema.dropTable('message').execute();
+  await db.schema.dropTable('templates').execute();
+  await db.schema.dropTable('sprint').execute();
+  await db.schema.dropTable('user').execute();
 }
