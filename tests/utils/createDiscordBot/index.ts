3c339ee488fa48a5835209cb92e1d3e78b6ea40d
Lukas Januskevicius <lukasjanuskevicius@MacBookPro.lan>
2025-02-03 16:00:21 +0200

    test(discord): update mock for discord bot

--- tests/utils/createDiscordBot/index.ts
+++ tests/utils/createDiscordBot/index.ts
@@ -1,21 +1,42 @@
 /* eslint-disable no-console */
-import { Client } from 'discord.js';
+import { Client, GuildMember, TextChannel } from 'discord.js';
 import { DiscordBot } from '@/modules/message/services/discord';
 import { ParsedGif } from '@/modules/message/services/giphy/schema';
 
+const mockUser = {
+  user: {
+    username: 'someUsername',
+    id: 'fakeUserId',
+    displayName: 'fakeUserId',
+  },
+} as GuildMember;
+
+const mockChannel = {
+  guild: {
+    members: {
+      fetch: async () =>
+        new Map<string, GuildMember>().set('fakeUserId', mockUser),
+    },
+  },
+} as unknown as TextChannel;
+
 export default (): DiscordBot => {
   const client = {} as Client;
+  const channel = mockChannel;
+
   const sendToChannel = async (
     message: string,
-    userName: string,
     gifImage?: ParsedGif,
   ): Promise<boolean> => {
     console.log('sendToChannel called with message:', message);
-    console.log('userName:', userName);
     console.log('gifImage:', gifImage);
-
     return true;
   };
 
-  return { client, sendToChannel };
+  const getUser = async () => {
+    console.log(`getUser called with`, mockUser.user);
+    return mockUser.user;
+  };
+
+  return { client, sendToChannel, getUser, channel };
 };
